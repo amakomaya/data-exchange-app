d0249a927a7364d7b4c748a0e135c05dbe1ebd68
Kripa <shresthakritisha1@gmail.com>
2024-09-09 10:23:16 +0545

    static payload for period added

--- src/components/edit/request-update/getRequestValues.js
+++ src/components/edit/request-update/getRequestValues.js
@@ -39,12 +39,45 @@ export const getRequestValuesFromForm = ({
 
     const filtersActuallyUsed =
         requestValues.filtersUsed && validFilters.length > 0
-
+        requestValues.peInfo = [
+            { id: "20240801", name: "2024-08-01" },
+            { id: "20240802", name: "2024-08-02" },
+            { id: "20240803", name: "2024-08-03" },
+            { id: "20240804", name: "2024-08-04" },
+            { id: "20240805", name: "2024-08-05" },
+            { id: "20240806", name: "2024-08-06" },
+            { id: "20240807", name: "2024-08-07" },
+            { id: "20240808", name: "2024-08-08" },
+            { id: "20240809", name: "2024-08-09" },
+            { id: "20240810", name: "2024-08-10" },
+            { id: "20240811", name: "2024-08-11" },
+            { id: "20240812", name: "2024-08-12" },
+            { id: "20240813", name: "2024-08-13" },
+            { id: "20240814", name: "2024-08-14" },
+            { id: "20240815", name: "2024-08-15" },
+            { id: "20240816", name: "2024-08-16" },
+            { id: "20240817", name: "2024-08-17" },
+            { id: "20240818", name: "2024-08-18" },
+            { id: "20240819", name: "2024-08-19" },
+            { id: "20240820", name: "2024-08-20" },
+            { id: "20240821", name: "2024-08-21" },
+            { id: "20240822", name: "2024-08-22" },
+            { id: "20240823", name: "2024-08-23" },
+            { id: "20240824", name: "2024-08-24" },
+            { id: "20240825", name: "2024-08-25" },
+            { id: "20240826", name: "2024-08-26" },
+            { id: "20240827", name: "2024-08-27" },
+            { id: "20240828", name: "2024-08-28" },
+            { id: "20240829", name: "2024-08-29" },
+            { id: "20240830", name: "2024-08-30" }
+            
+            
+        ]; 
     return {
         ...requestValues,
         name: requestValues.requestName,
         dx: requestValues?.dxInfo.map(({ id }) => id),
-        pe: requestValues?.peInfo.map(({ id }) => id),
+        pe: '208104',
         ou: requestValues?.ouInfo.map(({ id }) => id),
         visualization: requestValues.visualizationLinked
             ? requestValues?.visualizationInfo?.id
