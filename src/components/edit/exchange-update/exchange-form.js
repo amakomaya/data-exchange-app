57364ae8e47ced31cdd3a37c341bb3fd78c6368e
Kripa <shresthakritisha1@gmail.com>
2024-09-20 16:45:07 +0545

    fix: changed exchange data api and model

--- src/components/edit/exchange-update/exchange-form.js
+++ src/components/edit/exchange-update/exchange-form.js
@@ -12,6 +12,9 @@ import { ExchangeFormContents } from './exchange-form-contents.js'
 import styles from './exchange-form.module.css'
 import { getInitialValuesFromExchange } from './getExchangeValues.js'
 import { useRequests } from './useRequests.js'
+import { getExchangeValuesFromForm } from './getExchangeValues.js'
+import { config } from '../../../config';
+
 
 import { useUpdateExchange } from './useUpdateExchange.js'
 
@@ -59,29 +62,71 @@ export const ExchangeForm = ({ exchangeInfo, addMode }) => {
     const [isModalOpen, setModalOpen] = useState(false);
     const [modalData, setModalData] = useState([]); 
 
+    const [isDataModalOpen, setDataModalOpen] = useState(false);
+    const [DatamodalData, setDataModalData] = useState([]); 
+
     const handleCloseModal = () => {
         setModalOpen(false);
     };
 
-    const handleConfirm = async () => {
-        try {
-            const response = await fetch(`${baseUrl}/api/endpoint`, { // Update endpoint
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(modalData), // Sending modalData
-            });
+    const handleCloseDataModal = () => {
+        setDataModalOpen(false);
+    };
 
-            if (response.ok) {
-                const result = await response.json();
-                console.log('Data sent successfully:', result);
-                handleCloseModal();
-            } else {
-                console.error('Error sending data');
-            }
-        } catch (error) {
-            console.error('Failed to send data:', error);
+    const handleConfirm = async () => {
+       
+            const values =modalData.values;
+            const requests = modalData.requestsState
+            const dataValues = getExchangeValuesFromForm({
+                values,
+                requests,
+            });
+            const pe = dataValues?.source?.requests[0]?.peInfo[0].id
+            const targetUrl = dataValues?.target?.api.url
+            const username = dataValues?.target?.api.username
+            const password = dataValues?.target?.api.password
+
+            const request = dataValues?.source?.requests[0];
+            const dx = request?.dx.join(';'); 
+            const ou = request?.ou.join(';'); 
+            const periodData = request?.peInfo;
+            const startDate = periodData[0].startDate;
+            const endDate = periodData[0].endDate;
+            const baseUrl = config.baseUrl;            
+
+            const endpoint = '/api/analytics.json';
+            const url = `${baseUrl}${endpoint}?dimension=dx:${dx}&dimension=ou:${ou}&startDate=${startDate}&endDate=${endDate}&outputOrgUnitIdScheme=ATTRIBUTE:tL7ErP7HBel&outputIdScheme=ATTRIBUTE:b8KbU93phhz`;
+            const fetchResponse = await fetch(url);
+            if (fetchResponse.ok) {
+                const fetchedData = await fetchResponse.json();
+
+                const payload = {
+                    dataValues: fetchedData.rows.map(row => ({
+                        dataElement: row[0],
+                        period: pe,  
+                        orgUnit: row[1],
+                        value: parseInt(row[2]),  
+                    })),
+                };
+                const response = await fetch(`${targetUrl}api/dataValueSets`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Basic ' + btoa(`${username}:${password}`) 
+                    },
+                    body: JSON.stringify(payload),
+                });
+                if(response.ok){
+                    const data = await response.json();
+                    setDataModalData(data);
+                    setModalOpen(false);
+                    setDataModalOpen(true);
+                }
+                return response.ok
+                    ? console.log('Data sent successfully:', await response.json())
+                    : console.error('Error sending data:', response.statusText);
+            
+         
         }
     };
 
@@ -104,7 +149,12 @@ export const ExchangeForm = ({ exchangeInfo, addMode }) => {
                                 orgUnit:response.metaData.items[row[1]]?.name,
                                 total: row[2],
                             }));
-                            setModalData(formattedData); 
+                            const combinedData = {
+                                formattedData,
+                                values,         
+                                requestsState,  
+                            };
+                            setModalData(combinedData); 
                             setModalOpen(true); 
                         }
                     } catch (err) {
@@ -180,7 +230,6 @@ export const ExchangeForm = ({ exchangeInfo, addMode }) => {
                     </div>
                 )}
             </Form>
-                  
 
             {isModalOpen && (
                 <div
@@ -204,43 +253,49 @@ export const ExchangeForm = ({ exchangeInfo, addMode }) => {
                             width: '80%',
                             maxWidth: '600px',
                             boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)',
-                            overflowY: 'scroll', 
+                            overflowY: 'scroll',
                             maxHeight: '90vh',
                         }}
                     >
-                        {Object.entries(
-                            modalData.reduce((acc, data) => {
-                                if (!acc[data.orgUnit]) {
-                                    acc[data.orgUnit] = [];
-                                }
-                                acc[data.orgUnit].push(data);
-                                return acc;
-                            }, {})
-                        ).map(([orgUnit, records], orgIndex) => (
-                            <div key={orgIndex} style={{ marginBottom: '20px' }}>
-                                <h3>{orgUnit}</h3>
-                                <table style={{ width: '100%', borderCollapse: 'collapse' }}>
-                                    <thead>
-                                        <tr>
-                                            <th style={{ border: '1px solid #ddd', padding: '8px' }}>S.N</th>
-                                            <th style={{ border: '1px solid #ddd', padding: '8px' }}>Name</th>
-                                            <th style={{ border: '1px solid #ddd', padding: '8px' }}>Total</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {records.map((data, index) => (
-                                            <tr key={index}>
-                                                <td style={{ border: '1px solid #ddd', padding: '8px' }}>{index + 1}</td>
-                                                <td style={{ border: '1px solid #ddd', padding: '8px' }}>{data.name}</td>
-                                                <td style={{ border: '1px solid #ddd', padding: '8px' }}>{data.total}</td>
+                        {/* Check if modalData.formattedData exists and has length */}
+                        {modalData?.formattedData?.length > 0 ? (
+                            Object.entries(
+                                modalData.formattedData.reduce((acc, data) => {
+                                    if (!acc[data.orgUnit]) {
+                                        acc[data.orgUnit] = [];
+                                    }
+                                    acc[data.orgUnit].push(data);
+                                    return acc;
+                                }, {})
+                            ).map(([orgUnit, records], orgIndex) => (
+                                <div key={orgIndex} style={{ marginBottom: '20px' }}>
+                                    <h3>{orgUnit}</h3>
+                                    <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+                                        <thead>
+                                            <tr>
+                                                <th style={{ border: '1px solid #ddd', padding: '8px' }}>S.N</th>
+                                                <th style={{ border: '1px solid #ddd', padding: '8px' }}>Name</th>
+                                                <th style={{ border: '1px solid #ddd', padding: '8px' }}>Total</th>
                                             </tr>
-                                        ))}
-                                    </tbody>
-                                </table>
-                            </div>
-                        ))}
+                                        </thead>
+                                        <tbody>
+                                            {records.map((data, index) => (
+                                                <tr key={index}>
+                                                    <td style={{ border: '1px solid #ddd', padding: '8px' }}>{index + 1}</td>
+                                                    <td style={{ border: '1px solid #ddd', padding: '8px' }}>{data.name}</td>
+                                                    <td style={{ border: '1px solid #ddd', padding: '8px' }}>{data.total}</td>
+                                                </tr>
+                                            ))}
+                                        </tbody>
+                                    </table>
+                                </div>
+                            ))
+                        ) : (
+                            <div><strong>No data found</strong></div>
+                        )}
 
-                    <div style={{ marginTop: '15px', display: 'flex', justifyContent: 'flex-end' }}>
+
+                        <div style={{ marginTop: '15px', display: 'flex', justifyContent: 'flex-end' }}>
                             <Button
                                 style={{
                                     padding: '10px 15px',
@@ -270,6 +325,69 @@ export const ExchangeForm = ({ exchangeInfo, addMode }) => {
                     </div>
                 </div>
             )}
+
+
+            {isDataModalOpen && (
+                <div
+                    style={{
+                        position: 'fixed',
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        height: '100%',
+                        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <div
+                        style={{
+                            backgroundColor: 'white',
+                            padding: '20px',
+                            borderRadius: '8px',
+                            width: '80%',
+                            maxWidth: '600px',
+                            boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)',
+                            overflowY: 'scroll',
+                            maxHeight: '90vh',
+                        }}
+                    >
+                        <h3>Import Status: {DatamodalData.status}</h3>
+                        <p>Message: {DatamodalData.message}</p>
+                        <p>Description: {DatamodalData.response.description}</p>
+
+                        <h4>Import Count</h4>
+                        <ul>
+                            <li>Imported: {DatamodalData.response.importCount.imported}</li>
+                            <li>Updated: {DatamodalData.response.importCount.updated}</li>
+                            <li>Ignored: {DatamodalData.response.importCount.ignored}</li>
+                            <li>Deleted: {DatamodalData.response.importCount.deleted}</li>
+                        </ul>
+
+                        <h4>Other Details</h4>
+                        <p>Response Type: {DatamodalData.response.responseType}</p>
+                        <p>Data Set Complete: {DatamodalData.response.dataSetComplete === "false" ? "No" : "Yes"}</p>
+
+                        <div style={{ marginTop: '15px', display: 'flex', justifyContent: 'flex-end' }}>
+                            <Button
+                                style={{
+                                    padding: '10px 15px',
+                                    border: 'none',
+                                    borderRadius: '5px',
+                                    cursor: 'pointer',
+                                }}
+                                primary
+                                onClick={handleCloseDataModal}
+                            >
+                                {i18n.t('Close')}
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+
 
         </>
     );
