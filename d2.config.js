d0cb53e433d6a8e94bfc42f2a37ad1bdb6b4354d
Kripa <shresthakritisha1@gmail.com>
2024-09-29 11:33:05 +0545

    changed name

--- d2.config.js
+++ d2.config.js
@@ -1,8 +1,8 @@
 const config = {
-    id: '08012f21-0061-4e96-b49b-286e2743a8b4',
+    // id: '08012f21-0061-4e96-b49b-286e2743a8b4',
     type: 'app',
-    name: 'data-exchange',
-    title: 'Data Exchange',
+    name: 'tracker-to-aggregate',
+    title: 'Tracker to Aggregate AD to BS',
     minDHIS2Version: '2.39',
     coreApp: false,
     entryPoints: {
