28c2087b57d8402057a5d80ed9431c3bfea1c275
Kripa <shresthakritisha1@gmail.com>
2024-12-11 12:07:35 +0545

    changed app name

--- d2.config.js
+++ d2.config.js
@@ -1,8 +1,8 @@
 const config = {
     // id: '08012f21-0061-4e96-b49b-286e2743a8b4',
     type: 'app',
-    name: 'tracker-to-aggregate',
-    title: 'Tracker to Aggregate AD to BS',
+    name: 'hmis-report',
+    title: 'HMIS Report',
     minDHIS2Version: '2.39',
     coreApp: false,
     entryPoints: {
